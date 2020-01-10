e3af49c0c6818d5de42c831d951dbb85cb8c10af
sooyoungMMT <sooyoung.cho@mymusictaste.com>
2020-01-11 01:51:39 +0900

    <feat>(comm):Add projects dir

---
+++ projects/counting_character/src/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+
+fn counting_chars (str: String) -> i32 {
+    0
+}
+
+
+fn main () {
+    print!("Enter any string: ");
+    let mut my_string = Srring::new();
+    io::stdin()
+}
+
