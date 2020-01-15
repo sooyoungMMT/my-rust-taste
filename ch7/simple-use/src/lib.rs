bb69cf893c46f85c5b9ed4fe4f6d526a625a9e9c
sooyoungMMT <sooyoung.cho@mymusictaste.com>
2020-01-15 21:41:31 +0900

    <feat>(ch7):Add simple use example

---
+++ ch7/simple-use/src/lib.rs
@@ -0,0 +1,19 @@
+pub mod a {
+    pub mod series {
+        pub mod of {
+            pub fn nested_modules() {}
+        }
+    }
+}
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ ch7/simple-use/src/main.rs
@@ -0,0 +1,11 @@
+extern crate simple_use;
+
+use simple_use::a::series::of;
+use simple_use::a::series::of::nested_modules;
+
+fn main() {
+    simple_use::a::series::of::nested_modules();
+    of::nested_modules();
+    nested_modules();
+}
+
