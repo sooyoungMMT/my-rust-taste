5de241a7a66376fb1f106737e52bc913d89e958a
sooyoungMMT <sooyoung.cho@mymusictaste.com>
2020-01-15 21:55:36 +0900

    <fix>(ch7):Fix test code using use

--- ch7/communicator/src/lib.rs
+++ ch7/communicator/src/lib.rs
@@ -6,9 +6,11 @@ pub mod network;
 
 #[cfg(test)]
 mod tests {
+    use super::client;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
-        super::client::connect();
+        client::connect();
     }
 }
