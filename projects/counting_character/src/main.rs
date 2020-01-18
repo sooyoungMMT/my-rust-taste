1b79e7582c57731603860e91607904825200fa41
sooyoungMMT <sooyoung.cho@mymusictaste.com>
2020-01-19 02:19:43 +0900

    <feat>(cnt-char):Add code

--- projects/counting_character/src/main.rs
+++ projects/counting_character/src/main.rs
@@ -1,14 +1,33 @@
 use std::io;
 
 
-fn counting_chars (str: String) -> i32 {
-    0
+fn counting_chars (my_str: &String) -> i32 {
+    let mut count = 0;
+    loop {
+        match my_str.pop() {
+            Some(c) => {
+                count = count+1;
+            },
+            None => {
+                break;
+            }
+        }
+    }
+    count
 }
 
 
 fn main () {
-    print!("Enter any string: ");
-    let mut my_string = Srring::new();
-    io::stdin()
+    println!("Enter any string: ");
+    let mut my_string = String::new();
+    match io::stdin().read_line(&mut my_string) {
+        Ok(n) => {
+            let length = counting_chars(&my_string);
+            println!("length: {}", length);
+            println!("{} bytes read", n);
+            println!("{}", &my_string);
+        }
+        Err(error) => println!("error: {}", error),
+    }
 }
 
