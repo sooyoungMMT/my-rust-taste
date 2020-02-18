25578797e60a07bc1b4c22c03ccdf023f49dc933
sooyoungMMT <sooyoung.cho@mymusictaste.com>
2020-02-18 21:25:42 +0900

    feat(ch10): Add life annotations

---
+++ ch10/app/src/lifetime_examples.rs
@@ -0,0 +1,30 @@
+// fn longest (x: &str, y: &str) -> &str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+fn longest<'a> (x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn try_longest () {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("더 긴 문자열: {}", result);
+}
+
+
+
+
+pub fn run () {
+    try_longest();
+}
--- ch10/app/src/main.rs
+++ ch10/app/src/main.rs
@@ -1,7 +1,9 @@
 mod reduce_duplications;
 mod generic_example;
+mod lifetime_examples;
 
 fn main() {
     // reduce_duplications::run();
-    generic_example::run();
+    // generic_example::run();
+    lifetime_examples::run();
 }
