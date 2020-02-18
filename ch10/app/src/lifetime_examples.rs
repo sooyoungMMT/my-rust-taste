60999ae1b8e10978674703c10001bf4fdfc4cf63
sooyoungMMT <sooyoung.cho@mymusictaste.com>
2020-02-18 21:35:59 +0900

    feat(ch10): Add lifetime testing code

--- ch10/app/src/lifetime_examples.rs
+++ ch10/app/src/lifetime_examples.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code, unused_variables)]
 // fn longest (x: &str, y: &str) -> &str {
 //     if x.len() > y.len() {
 //         x
@@ -21,10 +22,20 @@ fn try_longest () {
     let result = longest(string1.as_str(), string2);
     println!("더 긴 문자열: {}", result);
 }
+fn try_with_diff_lifetime () {
+    let string1 = String::from("아주 아주 긴 문자열");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    // println!("더 긴 문자열: {}", result);
+}
 
 
 
 
 pub fn run () {
-    try_longest();
+    // try_longest();
+    try_with_diff_lifetime();
 }
