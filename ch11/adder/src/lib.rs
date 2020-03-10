5c739a89b9c30bca3665203e5a0f84a4ba1af813
sooyoungMMT <sooyoung.cho@mymusictaste.com>
2020-03-11 06:26:36 +0900

    test(ch11): Add ch11-1 tests

--- ch11/adder/src/lib.rs
+++ ch11/adder/src/lib.rs
@@ -1,14 +1,62 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+    // String::from("Hello")
+}
+
+pub struct Guess {
+    value: u32
+}
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+        Guess {
+            value
+        }
+    }
+}
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn exploration() {
-        assert_eq!(2 + 2, 5);
+        assert_eq!(2 + 2, 4);
     }
-
     #[test]
     fn another() {
-        panic!("테스트 실패!!");
+        // panic!("테스트 실패!!");
     }
 
+    //======
+
+    use super::*;
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`", result
+        );
+    }
+
+    //=======
+
+#[test]
+#[should_panic]
+fn greater_than_100() {
+    Guess::new(20);
+}
 
 }
