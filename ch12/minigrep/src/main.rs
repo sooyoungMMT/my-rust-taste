e678c7ce211734a7d3acb6b5e7ba4a2a440f7381
sooyoungMMT <sooyoung.cho@mymusictaste.com>
2020-03-15 20:48:35 +0900

    refactor(ch12): Refactor parsing config

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,15 +1,46 @@
+#![allow(dead_code)]
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    // let (query, filename) = parse_config(&args);
+    // let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("인수를 구문분석하는 동안 오류가 발생했습니다: {}", err);
+        process::exit(1);
+    });
     
-    println!("검색어: {}", query);
-    println!("대상 파일: {}", filename);
+    println!("검색어: {}", config.query);
+    println!("대상 파일: {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("파일을 읽지 못했습니다");
     println!("파일 내용:\n{}", contents);
+}
+
+struct Config { 
+    query: String,
+    filename: String
+}
+impl Config {
+    fn new (args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            // panic!("필요한 인수가 지정되지 않았습니다.");
+            return Err("필요한 인수가 지정되지 않았습니다.");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone(); 
+
+        Ok(Config { query, filename })
+    }
+}
+
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone(); 
+
+    Config { query, filename }
 }
